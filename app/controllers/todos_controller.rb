951dfb0c6b9c44f5df3a8a70f6efce3bd3a9e48a
Eric <eric.orellana92@gmail.com>
2015-04-15 16:51:01 -0400

    fake commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,10 +2,9 @@ class TodosController < ApplicationController
 
 
   def search
-
     fz = FuzzyMatch.new(Todo.all, :read => :name)
     @results = fz.find_all(params['/']['name'])
-
+    
     render 'results'
   end
 end
