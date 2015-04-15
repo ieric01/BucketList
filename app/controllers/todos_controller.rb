3ab2a9dc2095b4aeefd0aae2e06d62c53c6c7bfc
Ming Zhang <prongzhang@gmail.com>
2015-04-15 15:02:54 -0400

    added fuzzy search

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,8 @@ class TodosController < ApplicationController
 
   def search
 
-
+    fz = FuzzyMatch.new(Todo.all, :read => :name)
+    @results = fz.find_all(params['/']['name'])
 
     render 'results'
   end
