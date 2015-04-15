10e2f7196c7203404f489539734f68877626df10
Eric <eric.orellana92@gmail.com>
2015-04-15 09:09:44 -0400

    changed facebook api key and secret naming

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
   Rails.application.config.middleware.use OmniAuth::Builder do
   #WHEN APP LOADS ALL THE INITIALIZERS ARE SET FOR OMNIAUTH SERVICES
-  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET']
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
   # :scope => 'email,user_birthday,read_stream', :display => 'popup'
 end
--- db/migrate/20150414195215_create_categories.rb
+++ db/migrate/20150414195215_create_categories.rb
@@ -1,6 +1,6 @@
 class CreateCategories < ActiveRecord::Migration
   def change
-    create_table :categories do |t|
+    create_table :categories do |t| 
       t.timestamps null: false
     end
   end
