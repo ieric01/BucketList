13481e1dda80f9136e5f50c5fd2810b3e482f732
Ming Zhang <prongzhang@gmail.com>
2015-04-16 14:09:52 -0400

    added comments box. light styling.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,9 @@
 class CommentsController < ApplicationController
 
   def create
-
   Comment.create(:content => params['comment']['content'],
                  :todo_id => params['comment']['todo_id'],
                  :user_id => current_user.id)
-    binding.pry
    redirect_to (:back)
   end
 
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -75,6 +75,7 @@ class TodosController < ApplicationController
 
   def users_with_this_todo
     @todo_id = Todo.find_by(:name => params['todo']).id
+    @comments = Comment.where(:todo_id => @todo_id )
     @users_with_todo = Todo.find_by(:name => params['todo']).users
     @comment = Comment.new
     render 'users_with_this_todo'
