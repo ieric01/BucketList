a5bcb7c58b267f0c30c09bed2932c27b9241fbcc
Ming Zhang <prongzhang@gmail.com>
2015-04-22 09:04:30 -0400

    PM system, new layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get "mailbox/inbox" => "mailbox#inbox", as: :mailbox_inbox
+  get "mailbox/sent" => "mailbox#sent", as: :mailbox_sent
+  get "mailbox/trash" => "mailbox#trash", as: :mailbox_trash
+
+    resources :conversations do
+    member do
+      post :reply
+      post :trash
+      post :untrash
+    end
+  end
+
+
+
   resources :search_suggestions
   resources :comments
   post "/new_message", :to => 'mailbox#send_message'
