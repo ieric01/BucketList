4590e29004c1c8a05d145560ab98938c2349f060
Ming Zhang <prongzhang@gmail.com>
2015-04-23 11:22:31 -0400

    moved config ssl force true to production.rb

--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -1,6 +1,7 @@
 class SearchSuggestionsController < ApplicationController
   def index
     # render json: 
+    binding.pry
     results = SearchSuggestion.suggestions_scroll(search_params[:term])
     render :json => results
   end
