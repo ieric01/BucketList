e7ac414186d7a7241e26298409dca53ddb3aa975
Eric <eric.orellana92@gmail.com>
2015-04-30 18:04:34 -0400

    watchlist page bootstrap

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -40,6 +40,7 @@ class TodosController < ApplicationController
 
   def delete_todo_from_user
     if !(current_user.todos.include? Todo.find_by(:name => params['todo']))
+      binding.pry
       return redirect_to '/'
     end
 
