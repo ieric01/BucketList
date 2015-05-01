2f89bac6c7bd0cd6cf14b8acd72e71bdfce38468
hhassan1230 <hhassan@oswego.edu>
2015-05-01 12:33:40 -0400

    added more filter for coupon guess

--- app/models/coupon_giver.rb
+++ app/models/coupon_giver.rb
@@ -3,7 +3,7 @@ class CouponGiver
 	def self.distribute_deals(table, todo_title)
 		final_array = []
 		todo_title_array = todo_title.downcase.split
-		take_out_word = ["the", "a", "an", "to", "do", "and", "with", "in", "go", "by", "of", "own", "goto", "learn", "ride"]
+		take_out_word = ["the", "a", "my", "an", "to", "do", "and", "with", "in", "go", "by", "of", "own", "goto", "learn", "ride"]
 		take_out_word.each do |word|
 			if todo_title_array.include?(word)
 				todo_title_array.delete(word)
